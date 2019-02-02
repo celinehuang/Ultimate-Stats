faf0efe55231e319febaf3d798f62e53ff3bbed0
celinehuang <celine756@hotmail.com>
2019-02-02 17:24:51 -0500

    Updated dropdown selection

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import pandas as pd
 df = pd.read_csv(
     './USAU_Nationals_Player_Stats.csv')
 
+
 def generate_table(dataframe, max_rows=100):
     return html.Table(
         # Header
@@ -17,6 +18,7 @@ def generate_table(dataframe, max_rows=100):
         ]) for i in range(min(len(dataframe), max_rows))]
     )
 
+
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
@@ -26,42 +28,52 @@ app.layout = html.Div(children=[
     html.Div([
         html.Label('Division'),
         dcc.Dropdown(
-        id='division-dropdown',
-        options=[
-            {'label': 'Open', 'value': 'OPEN'},
-            {'label': 'Mixed', 'value': 'MIXED'},
-            {'label': 'Women', 'value': 'WOMEN'}
-        ],
-        value='OPEN'
-    )], style={'width': '15%', 'float': 'left', 'display': 'inline-block', 'margin': '5px'}),
+            id='division-dropdown',
+            options=[
+                {'label': 'Open', 'value': 'Open'},
+                {'label': 'Mixed', 'value': 'Mixed'},
+                {'label': 'Women', 'value': 'Women'}
+            ],
+            value='Open'
+        )], style={'width': '15%', 'float': 'left', 'display': 'inline-block', 'margin': '5px'}),
     html.Div([
         html.Label('Team'),
         dcc.Dropdown(
-        id='team-dropdown',
-        options=[
-            {'label': i, 'value': i} for i in df['Team'].unique()
-        ],
-    )], style={'width': '15%', 'float': 'left', 'margin': '5px'}),
+            id='team-dropdown',
+            options=[
+                {'label': i, 'value': i} for i in df['Team'].unique()
+            ],
+        )], style={'width': '15%', 'float': 'left', 'margin': '5px'}),
+
     html.Div([
         html.Label('Position'),
         dcc.Dropdown(
-        id='position-dropdown',
-        options=[
-            {'label': i, 'value': i} for i in df['Position'].unique()
-        ],
-    )], style={'width': '15%', 'float': 'left', 'margin': '5px'}),
+            id='position-dropdown',
+            options=[
+                {'label': i, 'value': i} for i in df['Position'].unique()
+            ],
+        )], style={'width': '15%', 'float': 'left', 'margin': '5px'}),
 
-    html.Div([generate_table(df)], id='chart', style={'float': 'left'}) 
+    html.Div([generate_table(df)], id='chart', style={'float': 'left'})
 ])
+
 
 @app.callback(
     dash.dependencies.Output('chart', 'children'),
-    [dash.dependencies.Input('division-dropdown', 'value'), 
-    dash.dependencies.Input('team-dropdown', 'value'),
-    dash.dependencies.Input('position-dropdown', 'value')])
+    [dash.dependencies.Input('division-dropdown', 'value'),
+     dash.dependencies.Input('team-dropdown', 'value'),
+     dash.dependencies.Input('position-dropdown', 'value')])
 def update_data(selected_division, selected_team, selected_position):
     filtered_df = df[df.Team == selected_team]
     return generate_table(filtered_df)
+
+
+@app.callback(
+    dash.dependencies.Output('team-dropdown', 'options'),
+    [dash.dependencies.Input('division-dropdown', 'value')])
+def update_dropdown(selected_division):
+    return [{'label': i, 'value': i} for i in df['Team'][df.Division == selected_division].unique()]
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
